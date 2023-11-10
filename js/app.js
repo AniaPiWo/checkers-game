6a51cf351610a4fd811e2f23d706ee7cbf03be66
AniaPiWo <snowflake@wp.pl>
2023-11-10 18:01:55 +0100

    init

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-import { Game } from "./modules/game.js";
+import { CheckersGame } from "./modules/game.js";
 import { Board } from "./modules/board.js";
+import { Player } from "./modules/player.js";
+import { CheckersDOMPrinter } from "./modules/printer.js";
 
 document.addEventListener("DOMContentLoaded", function () {
   const appContainerRef = document.getElementById("app");
@@ -7,9 +9,21 @@ document.addEventListener("DOMContentLoaded", function () {
   if (!appContainerRef) {
     throw new Error("App container not found!");
   }
+  //jak najmniej zaleznosci
+  //wszytsko zwiazane z plansza jest w board
+  const board = new Board();
+  //wszytsko zwiazane z drukowaniem jest w printer
+  const printer = new CheckersDOMPrinter({ appContainerRef });
+  //wszytsko zwiazane z gra jest w game
+  const game = new CheckersGame({ board, printer });
 
-  const board = new Board(appContainerRef);
-  const game = new Game({ board });
+  const player1 = new Player("Ania");
+  const player2 = new Player("Filip");
+
+  console.log(player1, player2);
+
+  game.addPlayer(player1, CheckersGame.getStartingPositionForWhite());
+  game.addPlayer(player2, CheckersGame.getStartingPositionForBlack());
 
   game.init();
 });
